bf4c105e66946f85841d796d4898f2f58160cd5d
Maryam Toubali <toubalimaryam98@gmail.com>
2025-05-06 00:01:55 +0100

    CourseExCh and ExercicesXP

---
+++ Week1/Day1/Exercices/ExercicesXP.py
@@ -0,0 +1,29 @@
+#ex1
+print("Hello world\n" * 4)    # print(.. , end="") to not add ligne /n in the end
+
+#ex2
+result = (99**3) * 8
+print(f"the result of: (99^3)*8 is: {result}")
+
+#ex3
+name = "Maryam"
+usname = input("What is your name? ")
+if usname.capitalize()==name.capitalize():
+    print("We have the same name.")
+else :
+    print(f"Nice to meet you, {usname}.")fe
+    
+#ex4
+
+#ex5
+
+#ex6
+
+#ex7
+
+#ex8
+
+
+
+
+
