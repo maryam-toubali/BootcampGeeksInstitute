c221c162fd06d1719972ec0e58013cb7fa46f958
Maryam Toubali <toubalimaryam98@gmail.com>
2025-05-06 18:12:13 +0100

    ExercicesXP&Summary

--- Week1/Day1/CourseExCh.py
+++ Week1/Day1/CourseExCh.py
@@ -1,4 +1,18 @@
 #
+a_tuple = (10, 20, 30, 40)
+a,b,c,d = a_tuple
+print(d)
+
+#
+list1 = [5, 10, 15, 20, 25, 50, 20]
+if 20 in list1 :
+    index = list1.index(20)
+    list1[index] = 200
+    print(list1)
+else:
+    print("You don't have the 20 number in your list")
+        
+#
 num = int(input('Give me please a number between 1 and 100: '))
 if num%3==0 and num%5==0 :
     print('FizzBuzz')
---
+++ testRun.py
@@ -0,0 +1,6 @@
+
+sandwich_orders = ["Tuna sandwich", "Pastrami sandwich", "Avocado sandwich", "Pastrami sandwich", "Egg sandwich", "Chicken sandwich", "Pastrami sandwich"]
+while 'Pastrami sandwich' in sandwich_orders :
+    sandwich_orders.remove('Pastrami sandwich')
+print(sandwich_orders)
+for sandwich_orders
